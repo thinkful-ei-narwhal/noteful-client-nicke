ada6ee1fc2bf54c663124b4358e19594e16638dd
nevelson <nevelson92@gmail.com>
2020-05-12 16:54:01 -0400

    Added functionality to backend

--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -1,15 +1,17 @@
 
-export const findFolder = (folders=[], folderId) =>
+export const findFolder = (folders = [], folderId) =>
   folders.find(folder => folder.id === folderId)
 
-export const findNote = (notes=[], noteId) =>
-  notes.find(note => note.id === noteId)
-
-export const getNotesForFolder = (notes=[], folderId) => (
-  (!folderId)
-    ? notes
-    : notes.filter(note => note.folderId === folderId)
+export const findNote = (notes = [], noteId) => (
+  notes.find(note => note.id === parseInt(noteId))
 )
 
-export const countNotesForFolder = (notes=[], folderId) =>
+
+export const getNotesForFolder = (notes = [], folderId) => (
+  (!folderId)
+    ? notes
+    : notes.filter(note => note.folderId === parseInt(folderId))
+)
+
+export const countNotesForFolder = (notes = [], folderId) =>
   notes.filter(note => note.folderId === folderId).length
